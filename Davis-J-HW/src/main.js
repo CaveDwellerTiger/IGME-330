89ebd3456e96a0672bd01dba016889a814fe00c8
CaveDwellerTiger <jsd1536@g.rit.edu>
2025-09-15 23:12:04 -0400

    Finished ultimate technobabble

--- Davis-J-HW/src/main.js
+++ Davis-J-HW/src/main.js
@@ -1,19 +1,13 @@
-"use strict";
-	
-const words1 = ["Acute", "Aft", "Anti-matter", "Bipolar", "Cargo", "Command", "Communication", "Computer", "Deuterium", "Dorsal", "Emergency", "Engineering", "Environmental", "Flight", "Fore", "Guidance", "Heat", "Impulse", "Increased", "Inertial", "Infinite", "Ionizing", "Isolinear", "Lateral", "Linear", "Matter", "Medical", "Navigational", "Optical", "Optimal", "Optional", "Personal", "Personnel", "Phased", "Reduced", "Science", "Ship's", "Shuttlecraft", "Structural", "Subspace", "Transporter", "Ventral"];
-	
-const words2 = ["Propulsion", "Dissipation", "Sensor", "Improbability", "Buffer", "Graviton", "Replicator", "Matter", "Anti-matter", "Organic", "Power", "Silicon", "Holographic", "Transient", "Integrity", "Plasma", "Fusion", "Control", "Access", "Auto", "Destruct", "Isolinear", "Transwarp", "Energy", "Medical", "Environmental", "Coil", "Impulse", "Warp", "Phaser", "Operating", "Photon", "Deflector", "Integrity", "Control", "Bridge", "Dampening", "Display", "Beam", "Quantum", "Baseline", "Input"];
-	
-const words3 = ["Chamber", "Interface", "Coil", "Polymer", "Biosphere", "Platform", "Thruster", "Deflector", "Replicator", "Tricorder", "Operation", "Array", "Matrix", "Grid", "Sensor", "Mode", "Panel", "Storage", "Conduit", "Pod", "Hatch", "Regulator", "Display", "Inverter", "Spectrum", "Generator", "Cloud", "Field", "Terminal", "Module", "Procedure", "System", "Diagnostic", "Device", "Beam", "Probe", "Bank", "Tie-In", "Facility", "Bay", "Indicator", "Cell"];
+import {randomElement} from "../src/utils.js";
+
+let words1 = [];
+
+let words2 = [];
+
+let words3 = [];
 
 const init = () => {
-    generate(1);
-    document.querySelector("#myButton").onclick = () => generate(1);
-    document.querySelector("#five-button").onclick = () => generate(5);
-}
-
-const randomElement = (array) => {
-    return array[Math.floor(Math.random() * array.length)];
+    loadBabble();
 }
 
 const generate = (num) => {
@@ -27,6 +21,36 @@ const generate = (num) => {
 
     }
     document.querySelector("#output").innerHTML = str;
+}
+
+const loadBabble = () => {
+    const url = "../data/babble-data.json";
+
+    const xhr = new XMLHttpRequest();
+
+    xhr.onload = (e) => {
+        babbleLoaded(e);
+    }
+
+    xhr.open("GET", url);
+    xhr.send();
+}
+
+const babbleLoaded = (e) => {
+    const text = e.target.responseText;
+
+    let json;
+    json = JSON.parse(text);
+
+    console.log(json);
+    words1 = json.words1;
+    words2 = json.words2;
+    words3 = json.words3;
+
+    document.querySelector("#my-button").onclick = () => generate(1);
+    document.querySelector("#five-button").onclick = () => generate(5);
+
+    generate(1);
 }
 
 init();
