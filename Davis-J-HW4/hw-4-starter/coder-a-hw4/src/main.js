872b011f608e08a8ed7fb4bd57ab13f231557a22
CaveDwellerTiger <jsd1536@g.rit.edu>
2025-12-08 21:23:17 -0500

    Added functioning favorite and delete buttons

---
+++ Davis-J-HW4/hw-4-starter/coder-a-hw4/src/main.js
@@ -0,0 +1,133 @@
+import * as map from "./map.js";
+import * as ajax from "./ajax.js";
+
+// I. Variables & constants
+// NB - it's easy to get [longitude,latitude] coordinates with this tool: http://geojson.io/
+const lnglatNYS = [-75.71615970715911, 43.025810763917775];
+const lnglatUSA = [-98.5696, 39.8282];
+let favoriteIds = [];
+let geojson;
+let currentId;
+
+
+// II. Functions
+const setupUI = () => {
+	// NYS Zoom 5.2
+	document.querySelector("#btn1").onclick = () => {
+		map.setZoomLevel(5.2);
+		map.setPitchAndBearing(0,0);
+		map.flyTo(lnglatNYS);
+	};
+	// NYS isometric view
+	document.querySelector("#btn2").onclick = () => {
+		map.setZoomLevel(5.5);
+		map.setPitchAndBearing(45,0);
+		map.flyTo(lnglatNYS);
+	}
+	// World zoom 0
+	document.querySelector("#btn3").onclick = () => {
+		map.setZoomLevel(3);
+		map.setPitchAndBearing(0,0);
+		map.flyTo(lnglatUSA);
+	}
+	// Favorites
+	refreshFavorites();
+	// Favorite Button
+	document.querySelector("#fav-button").onclick = () => addToFavorites(currentId);
+	// Delete Button
+	document.querySelector("#del-button").onclick = () => removeFavorite(currentId);
+
+}
+
+const getFeatureById = (id) => {
+	for(let obj of geojson.features){
+		if(obj.id == id){
+			return obj;
+		}
+	}
+}
+
+const showFeatureDetails = (id) => {
+	const feature = getFeatureById(id);
+	document.querySelector("#details-1").innerHTML = `Info for ${feature.properties.title}`;
+
+	let addressHtml = `<b>Address:</b> ${feature.properties.address}`;
+	addressHtml += `<p><b>Phone:</b> <a href="tel:${feature.properties.phone}">${feature.properties.phone}</a></p>`;
+	addressHtml += `<p><b>Website:</b> <a href="${feature.properties.url}">${feature.properties.url}</a></p>`
+	document.querySelector("#details-2").innerHTML = addressHtml;
+
+	// Favorite and Delete buttons
+	currentId = id;
+	document.querySelector("#fav-button").style.display = "inline"; // Show Favorite bttn by default
+	document.querySelector("#del-button").style.display = "none"; // Hide Delete button by default
+
+	// If this location is in the user's favorites, hide the Favorite button
+	// and show the Delete button.
+	for(let i = 0; i < favoriteIds.length; i++){
+		if(id == favoriteIds[i]){
+			document.querySelector("#fav-button").style.display = "none";
+			document.querySelector("#del-button").style.display = "inline";
+		}
+	}
+
+	document.querySelector("#details-3").innerHTML = `${feature.properties.description}`;
+	console.log(`showFeatureDetails - id=${id}`);
+};
+
+const refreshFavorites = () => {
+	const favoritesContainer = document.querySelector("#favorites-list");
+	favoritesContainer.innerHTML = "";
+	for (const id of favoriteIds){
+		favoritesContainer.appendChild(createFavoriteElement(id));
+	};
+};
+
+const createFavoriteElement = (id) => {
+	const feature = getFeatureById(id);
+	const a = document.createElement("a");
+	a.className = "panel-block";
+	a.id = feature.id;
+	a.onclick = () => {
+		showFeatureDetails(a.id);
+		map.setZoomLevel(6);
+		map.flyTo(feature.geometry.coordinates);
+	};
+	a.innerHTML = `
+	<span class="panel-icon">
+		<i class="fas fa-map-pin"></i>
+	</span>
+	${feature.properties.title}
+	`;
+	return a;
+};
+
+const addToFavorites = (id) => {
+	// Loop through favoriteIds. If the current ID is already in there, exit the function
+	for (let i = 0; i < favoriteIds.length; i++) {
+		if (id == favoriteIds[i]){
+			return null;
+		}
+	}
+	favoriteIds.push(id);
+	refreshFavorites();
+}
+
+const removeFavorite = (id) => {
+	let index = favoriteIds.indexOf(id);
+	if (index !== -1){
+		favoriteIds.splice(index, 1);
+		refreshFavorites();
+	}
+}
+
+const init = () => {
+	map.initMap(lnglatNYS);
+	ajax.downloadFile("data/parks.geojson", (str) => {
+		geojson = JSON.parse(str);
+		console.log(geojson);
+		map.addMarkersToMap(geojson, showFeatureDetails);
+		setupUI();
+	});
+};
+
+init();
